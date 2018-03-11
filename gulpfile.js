48e569d55cf0d5224142f432dc8bf1883e043325
sevak88 <philips226v>
2018-03-12 00:02:00 +0400

    gulp building

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp        = require("gulp"),
     sass        = require("gulp-sass"),
     concat      = require("gulp-concat"),
-    uglify      = require("gulp-uglifyjs");
+    uglify      = require("gulp-uglifyjs"),
+    imagemin    = require("gulp-imagemin");
 
 gulp.task("sass", function () {
     return gulp.src(["./app/app.scss"])
@@ -12,13 +13,53 @@ gulp.task("sass", function () {
 });
 
 gulp.task("scripts", function () {
-    return gulp.src(["./app/uikit/dist/js/uikit.js", "./app/uikit/dist/js/uikit-icons.js", "./app/uikit/dist/js/uikit-icons.js", "./app/app.js"])
+    return gulp.src([
+                "./app/uikit/dist/js/uikit.js",
+                "./app/uikit/dist/js/uikit-icons.js",
+                "./app/app.js"
+                ])
         .pipe(concat("app.min.js"))
         .pipe(uglify())
         .pipe(gulp.dest("./dist/js"))
 });
 
+gulp.task("html", function () {
+    return gulp.src(["./app/*.html"])
+        .pipe(gulp.dest("./dist"))
+});
+
+gulp.task("images", function () {
+    return gulp.src(["./app/images/**/*"])
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.jpegtran({progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+
+        ]))
+        .pipe(gulp.dest("./dist/images"))
+})
+
+gulp.task("build", ["scripts", "sass", "images", "html"], function () {
+    var buildFlags = gulp.src(["./app/flag-icon-css/flags/**/*.svg"])
+        .pipe(imagemin([
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]))
+        .pipe(gulp.dest("./dist/flags"));
+});
+
 gulp.task("watch", function () {
    gulp.watch(["./app/app.scss"], ["sass"]);
    gulp.watch(["./app/app.js"], ["scripts"]);
+   gulp.watch(["./app/*.html"], ["html"]);
 });
